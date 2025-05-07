c4b9a53f7efbad1dd30db43b572dd02badca7301
Birk Jefta Svensson <bis001@edu.zealand.dk>
2025-05-07 12:54:18 +0200

    kan modtage fra pi

--- ConsumeUDP.py
+++ ConsumeUDP.py
@@ -1,9 +1,26 @@
 import requests
 import json
+from socket import *
 
-URL = ""
+# til at kommunikerer med rapberry pi
+
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+serverAdress = ('', serverPort)
+clientSocket.bind(serverAdress)
 RequestTime = ""
+URL = "google.com"
 
-response = requests.get(URL + "/" + RequestTime)
-print(response)
+while True:
+    message, clientAddress = clientSocket.recvfrom(2048)
+    RequestTime = message.decode()
+    #Til at kommunikerer med rest server
+    #response = requests.get(URL + "/" + RequestTime)
+    print("RequestTime: ", (URL + "/" + RequestTime))
+
+
+
+
+
+
 
