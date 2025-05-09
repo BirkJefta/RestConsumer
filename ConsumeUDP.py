be09d8d2821dba27c7fbf11bdf7d906e01b38ecd
Finsby1 <bej002@edu.zealand.dk>
2025-05-09 11:18:49 +0200

    asdew

--- ConsumeUDP.py
+++ ConsumeUDP.py
@@ -1,25 +1,43 @@
 import requests
 import json
+import schedule
+import time
 from socket import *
 
-# til at kommunikerer med rapberry pi
 
-serverPort = 12000
-ServerSocket = socket(AF_INET, SOCK_DGRAM)
-serverAdress = ('', serverPort)
-ServerSocket.bind(serverAdress)
-RequestTime = ""
-URL = "google.com"
+hourNow = 0
+RequestTime = str(hourNow)
+
+
+def sendPriceCategory():
+    serverPort = 12000
+    broadcastAddress = ('255.255.255.255', serverPort)
+    ServerSocket = socket(AF_INET, SOCK_DGRAM)
+    ServerSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    URL = "google.com"
+    #Response = requests.get(URL + "/" + RequestTime)
+    Response = RequestTime
+    ServerSocket.sendto(Response.encode(), broadcastAddress)
+
+
+
+schedule.every(1).seconds.do(sendPriceCategory)
+
 
 while True:
-    Message, clientAddress = ServerSocket.recvfrom(2048)
-    RequestTime = Message.decode()
+    schedule.run_pending()
+    time.sleep(1)
+
+#while True:
+    
     #Til at kommunikerer med rest server
     #Response = requests.get(URL + "/" + RequestTime)
-    Response = "High"
-    print("RequestTime: ", (URL + "/" + RequestTime))
+    #Response = "High"
+    #print("RequestTime: ", (URL + "/" + RequestTime))
 
-    ServerSocket.sendto(Response.encode(), clientAddress)
+    #ServerSocket.sendto(Response.encode(), clientAddress)
+
+
 
 
 
