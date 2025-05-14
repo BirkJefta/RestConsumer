efd1afde41b063a9729d8eb62585b516c0414fc1
Birk Jefta Svensson <bis001@edu.zealand.dk>
2025-05-14 17:15:12 +0200

    Virker med socket test. skal stadig ændres til category. mangler fejlhåndtering og error besked til rødt lys.
    er afprøvet.
    priser er rigtige, mangler forskel på East og West som er hardcoded

--- ConsumeUDP.py
+++ ConsumeUDP.py
@@ -4,7 +4,7 @@ import schedule
 import time
 from socket import *
 
-Zone = "Vest"
+Zone = "East"
 URL = "https://etrest-eaf7c7abe8hkdgh8.northeurope-01.azurewebsites.net/api/Elpris/"
 
 #metoder
@@ -14,17 +14,19 @@ def RunSchedule():
     global jobref
     runcount = 0
     print("RunSchedule")
-    VestResponse = requests.get(URL + "fromAPI/Vest")
-    ØstResponse = requests.get(URL + "fromAPI/Øst")
-    print("Response1: " + str(VestResponse))
+    ApiCall = requests.get(URL + "fromAPI")
+    print("Response1: " + str(ApiCall))
     jobref = schedule.every().hour.do(hourlySendPrice)
+
+
+    
 # sørger for det maks er 24 timer herefter canceler den de forrige 24 timer hvorefter den starter forfra nederst
 def hourlySendPrice():
     global runcount, jobref
     sendPriceCategory()
     runcount += 1
     print("runcount: " + str(runcount)) 
-    if runcount > 24:
+    if runcount == 24:
         schedule.cancel_job(jobref)
         
 
@@ -36,20 +38,19 @@ def sendPriceCategory():
     clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     CurrentTime = time.localtime().tm_hour
     RequestTime = str(CurrentTime)
-    getURL = URL + Zone + "/" + RequestTime
-    response = requests.get(getURL)
+    getURL = str(URL + Zone + "/" + RequestTime)
+    response = requests.get(getURL)    
     print("Response: " + str(response))
-    #Skal laves om til category når den ikke er null mere og der er lavet en funktion til at udregne den
     dkK_per_kWh = response.json().get("dkK_per_kWh")
     print("Price " + str(dkK_per_kWh))
-    message = json.dumps(dkK_per_kWh)
+    message = json.dumps(dkK_per_kWh) 
     print("Message: " + message)
     clientSocket.sendto(message.encode(), broadcastAddress)
     clientSocket.close()
 
 #kør en gang hver dag.
 #skal starte klokken midnat
-schedule.every().day.at("14:37").do(RunSchedule)
+schedule.every().day.at("17:14").do(RunSchedule)
 
 #hoolder den tændt, så den kører schedule
 while True:
